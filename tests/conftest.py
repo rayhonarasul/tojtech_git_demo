afb52a342b0e707e0f61903232faee657513ab5d
Rayhona <test@gmail.com>
2026-07-12 00:00:03 -0400

    updatedconftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,20 +1,31 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 
+driver = None #usually this none is used when you're creating an object without any value yet, you can assign values later on like line 17
 
-@pytest.fixture(scope="class") #scope=class means the fixture will run once for the entire test class, not once for every test method
-def setup(request): #request is a built-in object that is used to configure the project, provides useful info
+def pytest_addoption(parser): #this allows to create custom arguments, for browsers, environments etc
+    #parser is a parameter that will help us to pass/parse the customized arguments that we'll create; translate it to the system
+    parser.addoption("--browser-name", action="store", default="chrome")
+    #with above lines you can switch between environments and browsers
+
+@pytest.fixture(scope="class") # scope = class means create the browser once for the entire test class, the browser opens once and closes after all tests finish
+def setup(request):
+    global driver
     option = Options()
     option.add_experimental_option("detach", True)
-    option.add_argument("start-maximized")
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=option)
-    driver.implicitly_wait(15)
+    browser_name = request.config.getoption("--browser-name")
+    if browser_name == "chrome":
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=option)
+    elif browser_name == "firefox":
+        driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+    driver.maximize_window()
+    driver.implicitly_wait(5)
     driver.get("http://staging.shopping.beeyor.com/shop")
-    request.cls.driver = driver #request allows to create customized properties , cls stands for class
-    #the above line is responsible for setting the driver's configurations on the class level then pass it to fixture
-    yield #pytest pauses the fixture and runs your tests, separates the setup code from teardown code
+    request.cls.driver = driver #take the browser we just created and give it to this entire test class so that every test method can access it through self.driver
+    yield
     driver.quit()
-    #above is teardown method
